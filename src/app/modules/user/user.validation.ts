18040d05c07fbde1e1e7d2b528196ebad321bcb8
monir6163 <monirhossain6163@gmail.com>
2026-02-10 13:27:48 +0600

    [shedule]-shedule-model

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -3,8 +3,7 @@ import * as z from "zod";
 export const UserValidation = {
   createUserSchema: z.object({
     body: z.object({
-      name: z.string().nonempty({ message: "Name is required" }),
-      email: z.string().optional(),
+      name: z.string().min(1, "Name is required"),
     }),
   }),
 };
