6d2d8fb64d017e5402aef08e801888ad70285487
monir6163 <monirhossain6163@gmail.com>
2026-02-16 16:41:38 +0600

    [multer-config]-setup

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -5,10 +5,21 @@ import { cloudinaryUpload } from "./cloudinary.config";
 const storage = new CloudinaryStorage({
   cloudinary: cloudinaryUpload,
   params: async (req, file) => {
+    const originalname = file.originalname;
+    const timestamp = Date.now();
+    const extension = originalname.split(".").pop()?.toLocaleLowerCase();
+    const filenameWithoutExt = originalname
+      .substring(0, originalname.lastIndexOf("."))
+      .replace(/\s+/g, "-")
+      // eslint-disable-next-line no-useless-escape
+      .replace(/[^a-zA-Z0-9\-]/g, "");
+    const finalFilename = `${filenameWithoutExt}-${timestamp}.${extension}`;
+    const folderName = extension === "pdf" ? "pdfs" : "images";
     return {
-      folder: "ph-health-care",
-      public_id: `${Date.now()}-${file.originalname}`,
-      allowed_formats: ["jpg", "jpeg", "png"],
+      folder: `healthcare/${folderName}`,
+      public_id: finalFilename,
+      allowed_formats: ["jpg", "jpeg", "png", "webp", "pdf"],
+      resource_type: "auto",
     };
   },
 });
