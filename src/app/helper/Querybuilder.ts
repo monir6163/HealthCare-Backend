39043d54a5bb9032cb00c0c2f17f936e7450c925
monir6163 <monirhossain6163@gmail.com>
2026-03-01 16:02:41 +0600

    [filter]-query

--- src/app/helper/Querybuilder.ts
+++ src/app/helper/Querybuilder.ts
@@ -72,4 +72,71 @@ export class QueryBuilder<
     }
     return this;
   }
+  filter(): this {
+    const { filterableFields } = this.config;
+    const excludedParams = [
+      "searchTerm",
+      "page",
+      "limit",
+      "sortBy",
+      "sortOrder",
+      "fields",
+      "includes",
+    ];
+
+    const filters: Record<string, unknown> = {};
+
+    Object.keys(this.queryParams).forEach((key) => {
+      if (
+        !excludedParams.includes(key) &&
+        filterableFields &&
+        filterableFields.includes(key)
+      ) {
+        filters[key] = this.queryParams[key];
+      }
+    });
+
+    const queryWhere = this.query.where as Record<string, unknown>;
+    const queryCountWhere = this.countQuery.where as Record<string, unknown>;
+    Object.keys(filters).forEach((key) => {
+      const value = filters[key];
+      if (value === undefined || value === "") return;
+
+      const isAllowedFilter =
+        !filterableFields ||
+        filterableFields.length === 0 ||
+        filterableFields.includes(key);
+
+      if (!isAllowedFilter) return;
+
+      if (key.includes(".")) {
+        const parts = key.split(".");
+        let currentQueryWhere = queryWhere;
+        let currentCountWhere = queryCountWhere;
+
+        for (let i = 0; i < parts.length - 1; i++) {
+          const part = parts[i];
+          if (!currentQueryWhere[part]) {
+            currentQueryWhere[part] = {};
+          }
+          if (!currentCountWhere[part]) {
+            currentCountWhere[part] = {};
+          }
+          currentQueryWhere = currentQueryWhere[part] as Record<
+            string,
+            unknown
+          >;
+          currentCountWhere = currentCountWhere[part] as Record<
+            string,
+            unknown
+          >;
+        }
+
+        const lastPart = parts[parts.length - 1];
+        currentQueryWhere[lastPart] = value;
+        currentCountWhere[lastPart] = value;
+      }
+    });
+    return this;
+  }
 }
