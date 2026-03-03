68d8b6cbf9e8748a03549faf5f332a55c00fda41
monir6163 <monirhossain6163@gmail.com>
2026-03-03 22:11:50 +0600

    [query]-builder

--- src/app/modules/doctor/doctor.controller.ts
+++ src/app/modules/doctor/doctor.controller.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { IQueryParams } from "../../helper/query.interface";
 import catchAsync from "../../shared/catchAsync";
+import sendResponse from "../../shared/sendResponse";
+import { doctorService } from "./doctor.service";
 
-const getAllDoctors = catchAsync(async (req: Request, res: Response) => {});
+const getAllDoctors = catchAsync(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await doctorService.getAllDoctors(query as IQueryParams);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Doctors retrieved successfully",
+    data: result,
+  });
+});
 
 export const doctorController = {
   getAllDoctors,
--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -1,4 +1,14 @@
-const getAllDoctors = async () => {};
+import { Prisma } from "../../../generated/prisma/client";
+import { IQueryParams } from "../../helper/query.interface";
+import { QueryBuilder } from "../../helper/Querybuilder";
+
+const getAllDoctors = async (query: IQueryParams) => {
+  const queryBuilder = new QueryBuilder(
+    Doctor,
+    Prisma.DoctorWhereInput,
+    Prisma.DoctorInclude,
+  );
+};
 
 export const doctorService = {
   getAllDoctors,
