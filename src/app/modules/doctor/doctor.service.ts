d87b51f1cbd92a30e01a0af1f1bb8726baba92ab
monir6163 <monirhossain6163@gmail.com>
2026-03-04 10:48:00 +0600

    [query]-builder

--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -1,13 +1,21 @@
-import { Prisma } from "../../../generated/prisma/client";
+import { Doctor, Prisma } from "../../../generated/prisma/client";
 import { IQueryParams } from "../../helper/query.interface";
 import { QueryBuilder } from "../../helper/Querybuilder";
+import { prisma } from "../../lib/prisma";
+import {
+  doctorFilterableFields,
+  doctorSearchableFields,
+} from "./doctor.constant";
 
 const getAllDoctors = async (query: IQueryParams) => {
-  const queryBuilder = new QueryBuilder(
+  const queryBuilder = new QueryBuilder<
     Doctor,
     Prisma.DoctorWhereInput,
-    Prisma.DoctorInclude,
-  );
+    Prisma.DoctorInclude
+  >(prisma.doctor, query, {
+    searchableFields: doctorSearchableFields,
+    filterableFields: doctorFilterableFields,
+  });
 };
 
 export const doctorService = {
